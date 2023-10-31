df5aab02cb025275a6677b7de9f7105c51432d25
Timmy Tran <timmy.tran@mrjaffesclass.com>
2023-10-31 11:33:00 -0700

    Initial commit

---
+++ MyScanner.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class scanners here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class MyScanner
+{
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+        
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+}
