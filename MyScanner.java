adc581229f1e3ba3b2ced8941aa58a8501b33468
Timmy Tran <timmy.tran@mrjaffesclass.com>
2023-11-01 11:38:28 -0700

    Commit message

--- MyScanner.java
+++ MyScanner.java
@@ -13,7 +13,9 @@ public class MyScanner
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-        
+        if (scanner.hasNext()) {
+            
+        }
         }
     }
 
